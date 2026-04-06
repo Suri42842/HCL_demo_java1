f2de42ad63d51c04ab53fb1352b8fb93868395dd
Suri42842 <psuriya2004@gmail.com>
2026-04-06 15:50:02 +0530

    new1

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Temperature{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(); 
+        double b=a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b-a);
+    }
+}
