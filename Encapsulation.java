1a03036e9fdfed4d6eee133a044c9f9800e3b9a3
Suri42842 <psuriya2004@gmail.com>
2026-04-07 09:52:11 +0530

    added Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,16 @@
+class Encapsulation{
+    private String get;
+    private int set;
+    public String getGet() {
+        return get;
+    }
+    public int getSet() {
+        return set;
+    }
+    public void setGet(String get) {
+        this.get = get;
+    }
+    public void setSet(int set) {
+        this.set = set;
+    }
+}
